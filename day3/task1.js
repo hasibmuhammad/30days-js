2f808e818e7f7c00724b37dfbeeb5f00741f275d
hasibmuhammad <hasibmu99@gmail.com>
2024-07-27 12:37:21 +0600

    add Positive, negative or zero

---
+++ day3/task1.js
@@ -0,0 +1,8 @@
+const number = 0;
+if (number > 0) {
+  console.log(`${number} is positive.`);
+} else if (number < 0) {
+  console.log(`${number} is negative.`);
+} else {
+  console.log(`The given number is 0`);
+}
