0f9fb39847f25ec6d7786796a7ebade1e402fdf2
hasibmuhammad <hasibmu99@gmail.com>
2024-07-27 23:35:42 +0600

    Enhanced object literals added

---
+++ day8/task8.js
@@ -0,0 +1,9 @@
+// TODO: VERFICATION
+
+const getInfo = (info) => info;
+const name = "john";
+const age = 20;
+
+const data = { name, age, getInfo };
+
+console.log(data);
---
+++ day8/task9.js
@@ -0,0 +1,9 @@
+const firstName = "john";
+const lastName = "Doe";
+
+const person = {
+  fullName: `${firstName} ${lastName}`,
+  age: 30,
+};
+
+console.log(person);
