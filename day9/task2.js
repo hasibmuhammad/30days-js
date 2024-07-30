9c9481ca8afc395fe7888525d7d98d4aea04ac4a
hasibmuhammad <hasibmu99@gmail.com>
2024-07-30 20:46:54 +0600

    create ul li element and append child

--- day9/task2.js
+++ day9/task2.js
@@ -1,4 +1,13 @@
 let divEl = document.createElement("div");
 divEl.textContent = "Hello world";
 
-document.getElementById("content").appendChild(divEl);
+const contentEl = document.getElementById("content");
+contentEl.appendChild(divEl);
+
+const ulEl = document.createElement("ul");
+const liEl = document.createElement("li");
+liEl.innerText = "Hello UL";
+
+ulEl.appendChild(liEl);
+
+contentEl.appendChild(ulEl);
