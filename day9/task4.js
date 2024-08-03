42397ecf2805dbe6f39abbf0c9752bcb2aba8360
hasibmuhammad <hasibmu99@gmail.com>
2024-08-03 11:58:13 +0600

    Add class remove class in the dom - added

--- day9/task4.js
+++ day9/task4.js
@@ -1,4 +1,8 @@
 const imgEl = document.getElementById("img");
 imgEl.setAttribute("alt", "new demo image");
 
+imgEl.classList.add("newClass", "overlay");
+
+imgEl.classList.remove("overlay");
+
 console.log(imgEl);
